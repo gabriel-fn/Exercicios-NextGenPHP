e0a31defb82459256d9f113fcdf015b00acc5772
Gabriel Nascimento <gabriel.mfn@gmail.com>
2024-12-26 17:42:41 -0300

    :white_check_mark: Create IsEvenTest

---
+++ Exercicio-Aula04/tests/unit/IsEvenTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use DifferDev\Exception\FailValidationException;
+use PHPUnit\Framework\Attributes\CoversClass;
+
+#[CoversClass(IsEven::class)]
+final class IsEvenTest extends \PHPUnit\Framework\TestCase
+{
+    public function testClassIsEvenShouldReturnTrue() 
+    {
+        $evenNumber = 300;
+        $isEven = new IsEven();
+        $result = $isEven->execute($evenNumber);
+
+        $this->assertTrue($result);
+    }
+
+    public function testClassIsEvenThrowException() 
+    {
+        $this->expectException(FailValidationException::class);
+        
+        $notEvenNumber = 301;
+        $isEven = new IsEven();
+        $isEven->execute($notEvenNumber);
+    }
+}
