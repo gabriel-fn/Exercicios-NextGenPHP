94f5b6a05d7631f1e921dc058411da52490636fc
Gabriel Nascimento <gabriel.mfn@gmail.com>
2024-12-26 21:18:53 -0300

    :fire: Remove static functions of Validator class

--- Exercicio-Aula04/src/Validator.php
+++ Exercicio-Aula04/src/Validator.php
@@ -15,37 +15,6 @@ class Validator
         $this->validations = array();
     }
 
-    public static function validateFloat(string $floatValue): bool
-    {
-        // Regex que valida se é string com número quebrado '3.4'
-        return (bool)preg_match('/^\d+\.\d+([eE][+-]?\d+)?$/', $floatValue);
-    }
-
-    public static function validateInteger(string $intValue): bool
-    {
-        return false === self::validateFloat($intValue);
-    }
-
-    /**
-     * Accepts true, false, TRUE, FALSE, 1 and 0 as string too.
-     * @param string $booleanValue
-     * @return bool
-     */
-    public static function validateBoolean(string $booleanValue): bool
-    {
-        return 1 >= preg_match('/^(true|false|1|0)$/i', $booleanValue);
-    }
-
-    public static function validateGreaterThan(float|string $number, float|string $compared): bool
-    {
-        return $number > $compared;
-    }
-
-    public static function validateEven(string $value): bool
-    {
-        return !((float)$value % 2);
-    }
-
     public function addValidation(ValidationHandler $validation): Validator 
     {
         $this->validations[] = $validation;
