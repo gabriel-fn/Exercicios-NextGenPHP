6f0a77daf1f6202b8a4d2d757fc9e67570b453d9
Gabriel Nascimento <gabriel.mfn@gmail.com>
2025-01-06 21:50:53 -0300

    :memo: Add exercício aula07

---
+++ Exercicio-Aula07/app/Http/Controllers/UsersController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+
+class UsersController extends Controller
+{
+    public function getAll(): JsonResponse
+    {
+        $users = User::all();
+        return response()->json($users);
+    }
+}
