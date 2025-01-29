06d818a3be4a0fb95c79cbc387e4e6534b730f81
Gabriel Nascimento <gabriel.mfn@gmail.com>
2025-01-29 13:03:26 -0300

    :construction: Create FindUserByIdUseCase

--- Exercicio-Aula07/app/Providers/AppServiceProvider.php
+++ Exercicio-Aula07/app/Providers/AppServiceProvider.php
@@ -4,9 +4,12 @@ namespace App\Providers;
 
 use Architecture\Application\Domain\Mapper\GenericObjectMapperInterface;
 use Architecture\Application\Domain\Repository\ReservationRepositoryInterface;
+use Architecture\Application\Domain\Repository\UserRepositoryInterface;
 use Architecture\Application\UseCase\FindReservationByIdUseCase;
+use Architecture\Application\UseCase\FindUserByIdUseCase;
 use Architecture\Infrastructure\Mapper\ObjectMapper;
 use Architecture\Infrastructure\Repository\ReservationRepository;
+use Architecture\Infrastructure\Repository\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -26,9 +29,21 @@ class AppServiceProvider extends ServiceProvider
             );
         });
 
+        $this->app->bind(UserRepositoryInterface::class, function ($app) {
+            return new UserRepository(
+                $app->make(GenericObjectMapperInterface::class)
+            );
+        });
+
         $this->app->bind(FindReservationByIdUseCase::class, function ($app) {
             return new FindReservationByIdUseCase(
                 $app->make(ReservationRepositoryInterface::class)
+            );        
+        });
+
+        $this->app->bind(FindUserByIdUseCase::class, function ($app) {
+            return new FindUserByIdUseCase(
+                $app->make(UserRepositoryInterface::class)
             );        
         });
     }
